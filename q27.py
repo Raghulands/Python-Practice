e618323a8fa1060a6f414318d721d5967cc0eb04
Ragu <111031929+Raghulands@users.noreply.github.com>
2026-02-24 08:37:09 +0530

    Add files via upload

---
+++ q27.py
@@ -0,0 +1,22 @@
+#calcclass
+class Calculator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        print(self.num1+self.num2)
+    def sub(self):
+        print(self.num1-self.num2)
+    def mul(self):
+        print(self.num1*self.num2)
+    def div(self):
+        print(self.num1/self.num2)
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+obj=Calculator(a,b)
+
+obj.add()
+obj.sub()
+obj.mul()
+obj.div()
